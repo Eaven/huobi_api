9965d968f6194de0f8f7cbabf477c790a7c200fd
Eaven <eaven@popstand.com>
2018-02-23 08:31:40 -0800

    get balances

--- lib/huobi_api/accounts.rb
+++ lib/huobi_api/accounts.rb
@@ -4,5 +4,14 @@ module HuobiApi
       endpoint = "/v1/account/accounts"
       get(endpoint, {})
     end
+
+    def balances(id)
+      options ={
+        'account-id': id
+      }
+
+      endpoint = "/v1/account/accounts/#{id}/balance"
+      get(endpoint, options)
+    end
   end
 end
